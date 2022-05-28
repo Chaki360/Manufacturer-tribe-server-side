28d70ee00a232d0d75b8e9df099033db7eee2ad4
unknown <fboy4069@gmail.com>
2022-05-28 13:10:59 +0600

    jwt token implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, CURSOR_FLAGS, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,7 +12,20 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_TRIBE}:${process.env.DB_PASS}@cluster0.6xcxy.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[0];
+    jwt.verify(token, '4dc149c85b84705dc44bd877d2d8792e825a8037a72b7d082a317209232fef9485eb85d420309af67e12542e155be39ff953119d34bf8f0e59bd932ce9ba47ba', function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 async function run() {
     try {
         await client.connect();
@@ -56,6 +70,14 @@ async function run() {
         });
         const orderCollection = client.db('Manufacturer-tribe').collection('orders');
         app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders)
+        });
+        app.get('/orders', async (req, res) => {
+            const authorization = req.headers.authorization;
             const query = {};
             const cursor = orderCollection.find(query);
             const orders = await cursor.toArray();
@@ -75,27 +97,24 @@ async function run() {
         });
         const userCollection = client.db('Manufacturer-tribe').collection('users');
         app.get('/user', async (req, res) => {
-            const users = await userCollection.find().toArray();
-            res.send(users);
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users)
         });
 
         app.put('/user/:email', async (req, res) => {
-            const user = req.body;
             const email = req.params.email;
-            const filter = { email: email };
+            console.log(email)
+            const user = req.body;
+            const filter = { email };
             const options = { upsert: true };
             const updateDoc = {
                 $set: user,
-            }
+            };
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            res.send(result);
-        });
-        app.delete('/user/:email', async (req, res) => {
-            const user = req.body;
-            const email = req.params.email;
-            const query = { _id: ObjectId(id) };
-            const result = await userCollection.deleteOne(query);
-            res.send(result);
+            const token = jwt.sign({ email }, '4dc149c85b84705dc44bd877d2d8792e825a8037a72b7d082a317209232fef9485eb85d420309af67e12542e155be39ff953119d34bf8f0e59bd932ce9ba47ba', { expiresIn: '1h' })
+            res.send({ result, token });
         });
 
 
